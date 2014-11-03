a91e8ffb02d1c9c5eeb273ce1d235c2bf895df99
Cameron Poole <Cameron.j.poole@gmail.com>
2014-11-03 13:52:24 +0800

    added form tests

--- DLB_activity_log/home/forms.py
+++ DLB_activity_log/home/forms.py
@@ -1,1 +1,14 @@
 #Forms stuff goes in here
+from django import forms
+
+class CreateDatasetForm(forms.Form):
+  """
+  Form for creating a new dataset
+  """
+  pass
+
+class EditDatasetForm(forms.Form):
+  """
+  Form for editing a new dataset
+  """
+  pass
