67ec237b0479b6ad3a63e02243a743cf058da8a6
Cameron Poole <Cameron.j.poole@gmail.com>
2014-11-03 15:20:13 +0800

    included dataset view

--- DLB_activity_log/home/views.py
+++ DLB_activity_log/home/views.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python2.7
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+from models import Dataset
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -9,6 +11,30 @@ class HomeView(TemplateView):
             'some_dynamic_value': 'This text comes from django view!',
         }
         return self.render_to_response(context)
+
+def create_dataset(request):
+    """
+    Create a new dataset view
+    """
+    return render_to_response('create_dataset.html')
+
+#not sure whether to use name or id
+def edit_dataset(request,datasetname):
+    """
+    Edit existing dataset
+    """
+    try:
+        ds_to_edit = Dataset.objects.get(name=datasetname)
+    except Dataset.DoesNotExist:
+        raise Http404
+    return render_to_response('edit_dataset.html')
+    
+def display_dataset(request,datasetname):
+    """
+    Return a complete overview for each dataset
+    """
+    return render_to_response('dataset_overview.html')
+    
 sample_queries = '''
 Which linkers have currently open formats or things 
 
