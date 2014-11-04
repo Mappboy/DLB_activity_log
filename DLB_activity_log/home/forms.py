99da277d41f92d36ce8718b0b029a61984608f76
Cameron Poole <Cameron.j.poole@gmail.com>
2014-11-04 14:56:32 +0800

    Now using inline form factories

--- DLB_activity_log/home/forms.py
+++ DLB_activity_log/home/forms.py
@@ -1,10 +1,20 @@
 #Forms stuff goes in here
 from django import forms
 from models import *
+from django.forms.models import inlineformset_factory
 
 # TODO: Create forms for most of our models
 
+def create_new_datasets():
+  retrun inlineformset_factory(CreateDatasetForm,CreateContact)
 
+class CreateContact(forms.ModelForm):
+  """
+  Form for adding a new contact
+  """
+  model = Client
+  fields = ['name','email']
+  
 class CreateDatasetForm(forms.ModelForm):
   """
   Form for creating a new dataset
